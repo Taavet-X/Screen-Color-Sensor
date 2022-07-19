8cf6b3d84f21830b9e9c210add6e00eb6e8863ea
David Estrada <77510617+Desth-X@users.noreply.github.com>
2022-07-19 11:36:47 -0500

    Add files via upload

---
+++ ScreenColorSensorListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Taavet
+ */
+public interface ScreenColorSensorListener {
+     public void onSensorEvent(Color color);
+}
